bd3cc1a6127e58dd2e4d96b1c086ef1f1aec573d
Sri Devi Abhirami <srideviabhirami.v@thoughtworks.com>
2021-05-24 01:09:52 +0530

    Add remaining balls

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -1,6 +1,6 @@
-import processing.core.PApplet;
 
-public class Ball extends PApplet{
+
+public class Ball{
     int position;
     int diameter;
     int ballSpeed;
--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -6,7 +6,8 @@ public class TryProcessing extends PApplet {
     public static final int HEIGHT = 480;
     public static final int DIAMETER = 10;
     int unitsperframe = 1;
-    Ball ball1;
+    public static final int BALLCOUNT=4;
+    Ball[] balls=new Ball[BALLCOUNT];
 
     public static void main(String[] args) {
         PApplet.main("TryProcessing",args);
@@ -20,17 +21,26 @@ public class TryProcessing extends PApplet {
 
     @Override
     public void setup() {
-        ball1 = new Ball(HEIGHT/5, DIAMETER, unitsperframe);
+        for(int i=1;i<=BALLCOUNT;i++) {
+            balls[i-1] = new Ball(i * HEIGHT / 5, DIAMETER, unitsperframe);
+            incrementUnitsPerFrame();
+        }
     }
 
     @Override
     public void draw() {
-        moveBall(ball1);
+        for(int i=0;i<BALLCOUNT;i++){
+            moveBall(balls[i]);
+        }
     }
 
     private void moveBall(Ball ball) {
-        ellipse(ball.ballSpeed,ball.position,ball.diameter,ball.diameter);
+        ellipse(ball.ballSpeed, ball.position, ball.diameter, ball.diameter);
         ball.incrementSpeed();
+    }
+
+    private void incrementUnitsPerFrame(){
+        unitsperframe+=1;
     }
 
 
