a5a141c0e45793cb91cd9a9a343c0d972af7f2a2
Sri Devi Abhirami <srideviabhirami.v@thoughtworks.com>
2021-05-24 00:53:26 +0530

    Add ball 1

---
+++ src/main/java/Ball.java
@@ -0,0 +1,19 @@
+import processing.core.PApplet;
+
+public class Ball extends PApplet{
+    int position;
+    int diameter;
+    int ballSpeed;
+    int unitsPerFrame;
+
+    public Ball( int position, int diameter,int unitsPerFrame) {
+        this.position = position;
+        this.diameter = diameter;
+        this.ballSpeed = 0;
+        this.unitsPerFrame = unitsPerFrame;
+    }
+
+    public void incrementSpeed(){
+        ballSpeed+=unitsPerFrame;
+    }
+}
--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -2,8 +2,11 @@ import processing.core.PApplet;
 
 public class TryProcessing extends PApplet {
 
-    public static final int WIDTH = 640;
+    public static final int WIDTH = 600;
     public static final int HEIGHT = 480;
+    public static final int DIAMETER = 10;
+    int unitsperframe = 1;
+    Ball ball1;
 
     public static void main(String[] args) {
         PApplet.main("TryProcessing",args);
@@ -16,7 +19,19 @@ public class TryProcessing extends PApplet {
     }
 
     @Override
-    public void setSize(int width, int height) {
-
+    public void setup() {
+        ball1 = new Ball(HEIGHT/5, DIAMETER, unitsperframe);
     }
+
+    @Override
+    public void draw() {
+        moveBall(ball1);
+    }
+
+    private void moveBall(Ball ball) {
+        ellipse(ball.ballSpeed,ball.position,ball.diameter,ball.diameter);
+        ball.incrementSpeed();
+    }
+
+
 }
